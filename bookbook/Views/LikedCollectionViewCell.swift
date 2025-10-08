b6e96c5036f12ef9a15dd5e15f7e823e117a2873
유정 <llyujay95@gmail.com>
2025-10-09 00:10:33 +0900

    feat: implement LikedCollectionViewCell for displaying liked book items

--- bookbook/Views/LikedCollectionViewCell.swift
+++ bookbook/Views/LikedCollectionViewCell.swift
@@ -9,5 +9,48 @@ import UIKit
 import SnapKit
 
 class LikedCollectionViewCell: UICollectionViewCell {
+
+    let bookStackView: UIStackView = {
+        let view = UIStackView()
+        view.bookStackView()
+        return view
+    }()
+
+    let bookImage: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+
+    let bookTitle: UILabel = {
+        let label = UILabel()
+        label.standardLabel()
+        label.numberOfLines = 1
+        return label
+    }()
+
+    let bookAuthor: UILabel = {
+        let label = UILabel()
+        label.subLabel()
+        label.numberOfLines = 1
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .white
+        configureUI()
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureUI() {
+        contentView.addSubview(bookStackView)
+        bookStackView.addArrangedSubviews([bookImage, bookTitle, bookAuthor])
+        bookStackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
 }
