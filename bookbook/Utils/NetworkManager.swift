f3c8fd587fc64c745d95a7187e215ccdd837744a
유정 <llyujay95@gmail.com>
2025-10-28 21:05:17 +0900

    feat: implement NetworkManager to handle all API requests added methods for book search, book list retrieval, and book detail fetching using Alamofire

--- bookbook/Utils/NetworkManager.swift
+++ bookbook/Utils/NetworkManager.swift
@@ -1,9 +1,3 @@
-//
-//  NetworkManager.swift
-//  bookbook
-//
-//  Created by 이유정 on 9/16/25.
-//
 
 import Foundation
 import Alamofire
@@ -12,24 +6,84 @@ final class NetworkManager {
     static let shared = NetworkManager()
     private init() { }
 
-    func searchBooks(query: String, completion: @escaping (Result<BookInfo, AFError>) -> Void) {
+    func searchBooks(query: String, category: Int, completion: @escaping (Result<BookInfo, AFError>) -> Void) {
         print(#function)
 
         let url = APIKey.baseURL
-        let headers: HTTPHeaders = [
-            "X-Naver-Client-Id": APIKey.naverClientId,
-            "X-Naver-Client-Secret": APIKey.naverClientSecret
-        ]
         let parameters: [String: Any] = [
-            "query": query,
-            "display": 100,
-            "start": 1
+            "TTBKey": APIKey.ttbKey,
+            "Output": "JS",
+            "Query": query,
+            "QueryType": "Keyword",
+            "SearchTarget": "Book",
+            "MaxResults": 20,
+            "Start": 1,
+            "Sort": "Accuracy",
+            "CategoryId": category,
+            "outofStockfilter": 1
         ]
 
         AF.request(url,
                    method: .get,
-                   parameters: parameters,
-                   headers: headers)
+                   parameters: parameters)
+        .validate(statusCode: 200..<300)
+        .responseDecodable(of: BookInfo.self) { response in
+            switch response.result {
+            case .success(let value):
+                print(value)
+                completion(.success(value))
+            case .failure(let error):
+                print(error)
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func bookLists(queryType: String, category: Int, completion: @escaping (Result<BookInfo, AFError>) -> Void) {
+        print("listRequest: \(queryType)")
+
+        let url = APIKey.baseURL
+        let parameters: [String: Any] = [
+            "TTBKey": APIKey.ttbKey,
+            "Output": "JS",
+            "QueryType": queryType,
+            "SearchTarget": "Book",
+            "CategoryId": category,
+            "MaxResults": 20,
+            "Start": 1,
+            "outofStockfilter": 1,
+            "Version": 20131101
+        ]
+
+        AF.request(url,
+                   method: .get,
+                   parameters: parameters)
+        .validate(statusCode: 200..<300)
+        .responseDecodable(of: BookInfo.self) { response in
+            switch response.result {
+            case .success(let value):
+                print(value)
+                completion(.success(value))
+            case .failure(let error):
+                print(error)
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func bookDetail(itemId: String, completion: @escaping (Result<BookInfo, AFError>) -> Void) {
+        print("detailRequest: \(itemId)")
+
+        let url = APIKey.baseURL
+        let parameters: [String: Any] = [
+            "TTBKey": APIKey.ttbKey,
+            "Output": "JS",
+            "ItemId": itemId,
+        ]
+
+        AF.request(url,
+                   method: .get,
+                   parameters: parameters)
         .validate(statusCode: 200..<300)
         .responseDecodable(of: BookInfo.self) { response in
             switch response.result {
