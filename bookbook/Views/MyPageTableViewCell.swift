cb34a086fc462a9c0b0c5a98204cee950fc957e8
유정 <llyujay95@gmail.com>
2025-11-06 23:57:26 +0900

    feat: implement MyPageTableViewCell UI created MyPageTableViewCell with label and chevron image layout for MyPageViewController

--- bookbook/Views/MyPageTableViewCell.swift
+++ bookbook/Views/MyPageTableViewCell.swift
@@ -1,24 +1,40 @@
-//
-//  MyPageTableViewCell.swift
-//  bookbook
-//
-//  Created by 이유정 on 9/20/25.
-//
 
 import UIKit
 import SnapKit
 
 class MyPageTableViewCell: UITableViewCell {
 
+    let menuName: UILabel = {
+        let label = UILabel()
+        label.textColor = .bk1
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 17)
+        return label
+    }()
+
+    let buttonImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "chevron.right")
+        image.tintColor = .bk1
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        configureUI()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    private func configureUI() {
+        contentView.addSubviews([menuName, buttonImage])
+        menuName.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+        }
+        buttonImage.snp.makeConstraints { make in
+            make.size.equalTo(17)
+            make.trailing.equalToSuperview().inset(16)
+            make.centerY.equalToSuperview()
+        }
     }
-
 }
