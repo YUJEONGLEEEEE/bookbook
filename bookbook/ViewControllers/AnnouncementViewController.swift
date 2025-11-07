85aac75b52b6a3255014e5f1dc2aacb82f3a305e
유정 <llyujay95@gmail.com>
2025-11-07 22:11:42 +0900

    feat: implement AnnouncementViewController layout (WorkInProcess) added initial layout for AnnouncementViewController to display notices and FAQs

---
+++ bookbook/ViewControllers/AnnouncementViewController.swift
@@ -0,0 +1,86 @@
+
+import UIKit
+import SnapKit
+
+class AnnouncementViewController: UIViewController {
+
+    private let noticeTableView: UITableView = {
+        let view = UITableView()
+        view.register(NoticeHeaderView.self, forHeaderFooterViewReuseIdentifier: "NoticeHeaderView")
+        view.register(NoticeATableViewCell.self, forCellReuseIdentifier: "NoticeATableViewCell")
+        return view
+    }()
+
+    private let qnaTableView: UITableView = {
+        let view = UITableView()
+        view.register(QnAHeaderView.self, forHeaderFooterViewReuseIdentifier: "QnAHeaderView")
+        view.register(QnATableViewCell.self, forCellReuseIdentifier: "QnATableViewCell")
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "읽담이 궁금해"
+        navigationItem.backButtonTitle = ""
+        configureUI()
+        noticeTableView.delegate = self
+        noticeTableView.dataSource = self
+    }
+
+    private func configureUI() {
+        view.backgroundColor = .customWh
+        view.addSubviews([noticeTableView, qnaTableView])
+    }
+}
+
+extension AnnouncementViewController: NoticeHeaderViewProtocol, UITableViewDelegate, UITableViewDataSource {
+
+    func headerViewButtonTapped(_ headerView: NoticeHeaderView) {
+        print(#function)
+        let vc = NoticeViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if tableView == noticeTableView {
+            let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "NoticeHeaderView") as! NoticeHeaderView
+            header.delegate = self
+            return header
+        } else if tableView == qnaTableView {
+            let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "QnAHeaderView") as! QnAHeaderView
+            return header
+        }
+        return nil
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == noticeTableView {
+            return  3
+        } else if tableView == qnaTableView {
+            return 5
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView == noticeTableView {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "NoticeATableViewCell", for: indexPath) as! NoticeATableViewCell
+            return cell
+        } else if tableView == qnaTableView {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "QnATableViewCell", for: indexPath) as! QnATableViewCell
+            return cell
+        }
+        fatalError("Unknown TableView")
+     }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView == noticeTableView {
+            print(#function)
+            let vc = NoticeViewController()
+            navigationController?.pushViewController(vc, animated: true)
+        } else if tableView == qnaTableView {
+            print(#function)
+            
+        }
+    }
+}
