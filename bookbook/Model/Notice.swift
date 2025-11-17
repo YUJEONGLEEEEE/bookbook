2df6987cd59be1b34d79e1fccea43c9d618219ef
유정 <llyujay95@gmail.com>
2025-11-17 11:15:04 +0900

    refactor: update Notice model content and adjust sample notice data

--- bookbook/Model/Notice.swift
+++ bookbook/Model/Notice.swift
@@ -8,8 +8,8 @@ struct Notice {
 }
 
 let noticeList: [Notice] = [
-    Notice(title: "읽담 앱 오픈!", date: "2025.11.17", description: "안녕하세요./n책과 대화하는 앱, 읽담입니다./n/n읽담이 드디어 오픈하였습니다./n맞춤 책 추천을 비롯하여 책 검색 및 내책장에 담기, 책한줄 적기 등 다양한 서비스로 여러분을 찾아갈 예정입니다./n계속해서 발전하는 읽담이 되겠습니다./n많은 이용 부탁드립니다./n/n감사합니다."),
-    Notice(title: "오류 수정", date: "2025.11.20", description: ""),
-    Notice(title: "앱 업데이트 1.0.1", date: "2025.12.01", description: ""),
-    Notice(title: "서비스 개선 안내", date: "2025.12.09", description: "")
+    Notice(title: "읽담 앱 오픈!", date: "2025.11.17", description: "안녕하세요./n책과 대화하는 앱, 읽담입니다./n/n오랫동안 준비해온 읽담이 드디어 여러분 곁에 첫 발을 내딛습니다./n/n책을 찾고, 담고, 마음에 남은 문장을 기록하는 순간들이 오롯이 여러부남ㄴ의 이야기로 쌓여가길 바라는 마음으로 만들었습니다./n/n따뜻한 관심과 많은 이용 부탁드립니다./n/n감사합니다.")
+//    Notice(title: "오류 수정", date: "2025.11.20", description: "안녕하세요. 읽담입니다./n2025년 11월 20일 제보된 "),
+//    Notice(title: "앱 업데이트 1.0.1", date: "2025.12.01", description: ""),
+//    Notice(title: "서비스 개선 안내", date: "2025.12.09", description: "")
 ]
